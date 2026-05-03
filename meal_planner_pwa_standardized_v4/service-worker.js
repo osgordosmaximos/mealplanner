68c316ba4056b8172b3c5741f3a15c181c602f6d
osgordosmaximos <osgordosmaximos@gmail.com>
2026-05-03 11:05:16 +0200

    Add files via upload

---
+++ meal_planner_pwa_standardized_v4/service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'meal-planner-standardized-v7';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/logo-leaf-180.png',
+  './icons/logo-leaf-192.png',
+  './icons/logo-leaf-512.png',
+  './icons/logo-leaf-1024.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request);
+    })
+  );
+});
